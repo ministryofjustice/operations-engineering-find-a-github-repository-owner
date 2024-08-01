e3bc831d4f58a9a2874d10f90e0265f33bb2e1ab
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-08-01 19:22:42 +0100

    ✨ Add DB Connection For Mapper Job

--- app/jobs/map_github_repositories_to_owners.py
+++ app/jobs/map_github_repositories_to_owners.py
@@ -1,5 +1,18 @@
+from app.main.services import database_service
+from app.main.services.database_service import DatabaseService
+import logging
+from app.main.config.app_config import app_config
+from app.main.config.logging_config import configure_logging
+
+
+logger = logging.getLogger(__name__)
+
+
 def main():
+    configure_logging(app_config.logging_level)
     print("Running...")
+    database_service = DatabaseService()
+    database_service.find_all_repositories()
 
 
 if __name__ == "__main__":
