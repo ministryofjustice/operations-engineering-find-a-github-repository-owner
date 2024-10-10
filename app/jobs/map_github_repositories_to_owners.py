e06778ccab78f8090ffbc78dbaa63ec5f64089ab
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-10-10 22:32:07 +0100

    ♻ Refactor Mapper Job to Remove Duplicate Logic

--- app/jobs/map_github_repositories_to_owners.py
+++ app/jobs/map_github_repositories_to_owners.py
@@ -19,41 +19,68 @@ def contains_one_or_more(values: list[str], lists_to_check: list[list[str]]) -> 
 
 
 def main(
-    owners=[{"name": "HMPPS", "teams": ["HMPPS Developers"], "prefix": "hmpps-"}],
-    hmpps_teams=["HMPPS Developers"],
-    laa_teams=[
-        "LAA Technical Architects",
-        "LAA Developers",
-        "LAA Crime Apps team",
-        "LAA Crime Apply",
-        "laa-eligibility-platform",
-        "LAA Get Access",
-        "LAA Payments and Billing",
+    owners=[
+        {
+            "name": "HMPPS",
+            "teams": ["HMPPS Developers"],
+            "prefix": "hmpps-",
+        },
+        {
+            "name": "LAA",
+            "teams": [
+                "LAA Technical Architects",
+                "LAA Developers",
+                "LAA Crime Apps team",
+                "LAA Crime Apply",
+                "laa-eligibility-platform",
+                "LAA Get Access",
+                "LAA Payments and Billing",
+            ],
+            "prefix": "laa-",
+        },
+        {
+            "name": "OPG",
+            "teams": ["OPG"],
+            "prefix": "opg-",
+        },
+        {
+            "name": "CICA",
+            "teams": ["CICA"],
+            "prefix": "cica-",
+        },
+        {
+            "name": "Central Digital",
+            "teams": ["Central Digital Product Team", "tactical-products"],
+        },
+        {
+            "name": "Platforms and Architecture",
+            "teams": [
+                ### Hosting Platforms
+                "modernisation-platform",
+                "operations-engineering",
+                "aws-root-account-admin-team",
+                "WebOps",  # Cloud Platform
+                "Studio Webops",  # Digital Studio Operations (DSO)
+                ### Data Platforms
+                "analytical-platform",
+                "data-engineering",
+                "analytics-hq",
+                "data-catalogue",
+                "data-platform",
+                "data-and-analytics-engineering",
+                "observability-platform",
+                ### Publishing Platforms
+                "Form Builder",
+                "Hale platform",
+                "JOTW Content Devs",
+            ],
+            "prefix": "bichard7",
+        },
+        {
+            "name": "Tech Services",
+            "teams": ["nvvs-devops-admins", "moj-official-techops"],
+        },
     ],
-    opg_teams=["OPG"],
-    cica_teams=["CICA"],
-    central_digital_teams=["Central Digital Product Team", "tactical-products"],
-    platforms_and_architecture_teams=[
-        ### Hosting Platforms
-        "modernisation-platform",
-        "operations-engineering",
-        "aws-root-account-admin-team",
-        "WebOps",  # Cloud Platform
-        "Studio Webops",  # Digital Studio Operations (DSO)
-        ### Data Platforms
-        "analytical-platform",
-        "data-engineering",
-        "analytics-hq",
-        "data-catalogue",
-        "data-platform",
-        "data-and-analytics-engineering",
-        "observability-platform",
-        ### Publishing Platforms
-        "Form Builder",
-        "Hale platform",
-        "JOTW Content Devs",
-    ],
-    tech_services_teams=["nvvs-devops-admins", "moj-official-techops"],
 ):
     configure_logging(app_config.logging_level)
     logger.info("Running...")
@@ -62,141 +89,52 @@ def main(
     github_service = GithubService(app_config.github.token)
     repositories = github_service.get_all_repositories()
 
-    hmpps_owner_id, _ = database_service.find_owner_by_name("HMPPS")[0]
-    laa_owner_id, _ = database_service.find_owner_by_name("LAA")[0]
-    opg_owner_id, _ = database_service.find_owner_by_name("OPG")[0]
-    cica_owner_id, _ = database_service.find_owner_by_name("CICA")[0]
-    central_digital_owner_id, _ = database_service.find_owner_by_name(
-        "Central Digital"
-    )[0]
-    platforms_and_architecture_owner_id, _ = database_service.find_owner_by_name(
-        "Platforms and Architecture"
-    )[0]
-    tech_services_owner_id, _ = database_service.find_owner_by_name("Tech Services")[0]
+    for owner in owners:
+        name = owner["name"]
+        teams = owner["teams"]
+        prefix = owner.get("prefix")
 
-    for repository in repositories:
-        github_teams_with_admin_access = repository["github_teams_with_admin_access"]
-        github_teams_with_admin_access_parents = repository[
-            "github_teams_with_admin_access_parents"
-        ]
-        github_teams_with_any_access = repository["github_teams_with_any_access"]
-        github_teams_with_any_access_parents = repository[
-            "github_teams_with_any_access_parents"
-        ]
-        repository_name = repository["name"]
-        teams_with_admin_access = [
-            github_teams_with_admin_access,
-            github_teams_with_admin_access_parents,
-        ]
-        teams_with_any_access = [
-            github_teams_with_any_access,
-            github_teams_with_any_access_parents,
-        ]
+        owner_id, _ = database_service.find_owner_by_name(name)[0]
 
-        # HMPPS Digital
-        if contains_one_or_more(hmpps_teams, teams_with_admin_access):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, hmpps_owner_id, "ADMIN_ACCESS"
-            )
-        elif (
-            contains_one_or_more(hmpps_teams, teams_with_any_access)
-            # Fuzzy Matches 👇
-            or repository_name.startswith("hmpps-")
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, hmpps_owner_id
-            )
-
-        # LAA Digital
-        if contains_one_or_more(laa_teams, teams_with_admin_access):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, laa_owner_id, "ADMIN_ACCESS"
-            )
-        elif (
-            contains_one_or_more(laa_teams, teams_with_any_access)
-            # Fuzzy Matches 👇
-            or repository_name.startswith("laa-")
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, laa_owner_id
-            )
-
-        # OPG Digital
-        if contains_one_or_more(opg_teams, teams_with_admin_access):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, opg_owner_id, "ADMIN_ACCESS"
-            )
-        elif (
-            contains_one_or_more(opg_teams, teams_with_any_access)
-            # Fuzzy Matches 👇
-            or repository_name.startswith("opg-")
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, opg_owner_id
-            )
-
-        # CICA Digital
-        if contains_one_or_more(cica_teams, teams_with_admin_access):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, cica_owner_id, "ADMIN_ACCESS"
-            )
-        elif (
-            contains_one_or_more(cica_teams, teams_with_any_access)
-            # Fuzzy Matches 👇
-            or repository_name.startswith("cica-")
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, cica_owner_id
-            )
-
-        # Central Digital
-        if contains_one_or_more(central_digital_teams, teams_with_admin_access):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, central_digital_owner_id, "ADMIN_ACCESS"
-            )
-        elif contains_one_or_more(central_digital_teams, teams_with_any_access):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, central_digital_owner_id
-            )
-
-        # Platforms and Architecture https://peoplefinder.service.gov.uk/teams/platforms
-        if contains_one_or_more(
-            platforms_and_architecture_teams, github_teams_with_admin_access
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, platforms_and_architecture_owner_id, "ADMIN_ACCESS"
-            )
-        elif (
-            ## Platforms
-            contains_one_or_more(
-                platforms_and_architecture_teams,
+        for repository in repositories:
+            github_teams_with_admin_access = repository[
+                "github_teams_with_admin_access"
+            ]
+            github_teams_with_admin_access_parents = repository[
+                "github_teams_with_admin_access_parents"
+            ]
+            github_teams_with_any_access = repository["github_teams_with_any_access"]
+            github_teams_with_any_access_parents = repository[
+                "github_teams_with_any_access_parents"
+            ]
+            repository_name = repository["name"]
+            teams_with_admin_access = [
+                github_teams_with_admin_access,
+                github_teams_with_admin_access_parents,
+            ]
+            teams_with_any_access = [
                 github_teams_with_any_access,
-            )
-            ## Criminal Justice Services
-            # Fuzzy Matches 👇
-            or repository_name.startswith("bichard7")
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, platforms_and_architecture_owner_id
+                github_teams_with_any_access_parents,
+            ]
+            repository_name_starts_with_prefix = (
+                repository_name.startswith(prefix) if prefix is not None else False
             )
 
-        # Tech Services
-        if contains_one_or_more(
-            tech_services_teams,
-            github_teams_with_admin_access,
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, tech_services_owner_id, "ADMIN_ACCESS"
-            )
-        elif contains_one_or_more(
-            tech_services_teams,
-            github_teams_with_any_access,
-        ):
-            database_service.add_relationship_between_asset_and_owner(
-                repository_name, tech_services_owner_id
-            )
+            if contains_one_or_more(teams, teams_with_admin_access):
+                database_service.add_relationship_between_asset_and_owner(
+                    repository_name, owner_id, "ADMIN_ACCESS"
+                )
+            elif (
+                contains_one_or_more(teams, teams_with_any_access)
+                or repository_name_starts_with_prefix
+            ):
+                database_service.add_relationship_between_asset_and_owner(
+                    repository_name, owner_id
+                )
 
-        database_service.add_asset_if_name_does_not_exist("REPOSITORY", repository_name)
+            database_service.add_asset_if_name_does_not_exist(
+                "REPOSITORY", repository_name
+            )
 
     logger.info("Complete!")
 
