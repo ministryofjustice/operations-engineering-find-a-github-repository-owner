bfe19838363b4f6aa90f5643998e931fc2427df6
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2024-10-31 10:27:29 +0000

    ✨ Add New Business Unit Dashboard (#16)

---
+++ app/main/routes/organisation.py
@@ -0,0 +1,130 @@
+import logging
+from app.main.middleware.auth import requires_auth
+from app.main.services.database_service import DatabaseService
+import plotly.express as px
+import pandas as pd
+from flask import Blueprint, render_template, request
+from collections import Counter
+
+logger = logging.getLogger(__name__)
+
+organisation_route = Blueprint("organisation", __name__)
+
+
+def generate_pie_chart_of_admin_access(repositories: list[dict]):
+    dataframe = pd.DataFrame(repositories)
+    dataframe["owners"] = dataframe["owners"].apply(
+        lambda owners: [
+            owner["relationship_type"]
+            for owner in owners
+            if "relationship_type" in owner
+        ]
+        if owners
+        else ["No relationships"]
+    )
+    all_owners = [owner for owners_list in dataframe["owners"] for owner in owners_list]
+    owner_counts = pd.DataFrame(
+        Counter(all_owners).items(), columns=["owners", "count"]
+    )
+
+    fig = px.pie(
+        owner_counts,
+        names="owners",
+        values="count",
+    )
+    return fig.to_html(full_html=False)
+
+
+def generate_pie_chart(repositories: list[dict]):
+    dataframe = pd.DataFrame(repositories)
+    dataframe["owners"] = dataframe["owners"].apply(
+        lambda owners: [
+            owner["owner_name"] for owner in owners if "owner_name" in owner
+        ]
+        if owners
+        else ["No owner"]
+    )
+    all_owners = [owner for owners_list in dataframe["owners"] for owner in owners_list]
+    owner_counts = pd.DataFrame(
+        Counter(all_owners).items(), columns=["owners", "count"]
+    )
+
+    fig = px.pie(
+        owner_counts,
+        names="owners",
+        values="count",
+    )
+    return fig.to_html(full_html=False)
+
+
+@organisation_route.route("/", methods=["GET"])
+@requires_auth
+def index():
+    database_service = DatabaseService()
+    repositories = database_service.find_all_repositories()
+    owners = database_service.find_all_owners()
+    repository_name = request.args.get("repository-name")
+    selected_owners = request.args.getlist("owner")
+    selected_access_levels = request.args.getlist("access-levels")
+
+    filtered_repositories_by_name = []
+    if repository_name:
+        for repository in repositories:
+            if repository_name and repository_name in repository["name"]:
+                filtered_repositories_by_name = filtered_repositories_by_name + [
+                    repository
+                ]
+    else:
+        filtered_repositories_by_name = repositories
+
+    filtered_repositories = []
+    if selected_owners:
+        for repository in filtered_repositories_by_name:
+            if "NO_OWNER" in selected_owners and not repository["owners"]:
+                filtered_repositories = filtered_repositories + [repository]
+                continue
+
+            selected_owners_full = []
+            for owner in repository["owners"]:
+                if owner["owner_name"] in selected_owners:
+                    selected_owners_full += [owner]
+            for owner in selected_owners_full:
+                has_admin_access = (
+                    True if "ADMIN_ACCESS" in owner.get("relationship_type") else False
+                )
+
+                if (
+                    "ADMIN" in selected_access_levels
+                    and has_admin_access
+                    or "OTHER" in selected_access_levels
+                    and not has_admin_access
+                ):
+                    repositories_filtered_by_access_level = filtered_repositories + [
+                        repository
+                    ]
+                    filtered_repositories = filtered_repositories + [repository]
+                    break
+    else:
+        filtered_repositories = filtered_repositories_by_name
+
+    repositories_filtered_by_access_level = filtered_repositories
+
+    return render_template(
+        "pages/organisation.html",
+        repositories=repositories_filtered_by_access_level,
+        pie_chart=generate_pie_chart(repositories_filtered_by_access_level)
+        if repositories_filtered_by_access_level
+        else None,
+        pie_chart_of_admin_access=generate_pie_chart_of_admin_access(
+            repositories_filtered_by_access_level
+        )
+        if repositories_filtered_by_access_level
+        else None,
+        repository_name=repository_name if repository_name else "",
+        selected_owners=selected_owners if selected_owners else owners + ["NO_OWNER"],
+        owners=owners,
+        access_levels=["ADMIN", "OTHER"],
+        selected_access_levels=selected_access_levels
+        if selected_access_levels
+        else ["ADMIN", "OTHER"],
+    )
--- app/main/routes/owner.py
+++ app/main/routes/owner.py
@@ -3,7 +3,7 @@ from app.main.middleware.auth import requires_auth
 from app.main.services.database_service import DatabaseService
 import plotly.express as px
 import pandas as pd
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, abort
 from collections import Counter
 
 logger = logging.getLogger(__name__)
@@ -42,48 +42,45 @@ def index():
     repositories = database_service.find_all_repositories()
     owners = database_service.find_all_owners()
     repository_name = request.args.get("repository-name")
-    selected_owner = request.args.get("owner")
+    selected_owner = request.args.get("owner") or "NO_OWNER"
     selected_access_levels = request.args.getlist("access-levels")
     exclude_other_relationships_where_admins_exist = bool(
         request.args.get("exclude_other_relationships_where_admins_exist")
     )
     filtered_repositories = []
-    if selected_owner:
-        for repository in repositories:
-            if "NO_OWNER" in selected_owner and not repository["owners"]:
-                filtered_repositories = filtered_repositories + [repository]
-                continue
+    for repository in repositories:
+        if "NO_OWNER" in selected_owner and not repository["owners"]:
+            filtered_repositories = filtered_repositories + [repository]
+            continue
 
-            owners_with_admin_access = [
-                o
-                for o in repository["owners"]
-                if "ADMIN_ACCESS" in o.get("relationship_type")
-            ]
+        owners_with_admin_access = [
+            o
+            for o in repository["owners"]
+            if "ADMIN_ACCESS" in o.get("relationship_type")
+        ]
 
-            for owner in repository["owners"]:
-                if owner["owner_name"] == selected_owner:
-                    has_atleast_one_admin = bool(len(owners_with_admin_access) > 0)
-                    has_admin_access = bool(
-                        "ADMIN_ACCESS" in owner.get("relationship_type")
+        for owner in repository["owners"]:
+            if owner["owner_name"] == selected_owner:
+                has_atleast_one_admin = bool(len(owners_with_admin_access) > 0)
+                has_admin_access = bool(
+                    "ADMIN_ACCESS" in owner.get("relationship_type")
+                )
+                has_other_access = bool("OTHER" in owner.get("relationship_type"))
+
+                include_admin_access = bool(
+                    "ADMIN" in selected_access_levels and has_admin_access
+                )
+                include_other_access = bool(
+                    "OTHER" in selected_access_levels
+                    and has_other_access
+                    and not (
+                        exclude_other_relationships_where_admins_exist
+                        and has_atleast_one_admin
                     )
-                    has_other_access = bool("OTHER" in owner.get("relationship_type"))
+                )
 
-                    include_admin_access = bool(
-                        "ADMIN" in selected_access_levels and has_admin_access
-                    )
-                    include_other_access = bool(
-                        "OTHER" in selected_access_levels
-                        and has_other_access
-                        and not (
-                            exclude_other_relationships_where_admins_exist
-                            and has_atleast_one_admin
-                        )
-                    )
-
-                    if include_admin_access or include_other_access:
-                        filtered_repositories += [repository]
-    else:
-        filtered_repositories = repositories
+                if include_admin_access or include_other_access:
+                    filtered_repositories += [repository]
 
     filtered_repositories_by_name = []
     if repository_name:
@@ -106,11 +103,40 @@ def index():
         if repositories_filtered_by_access_level
         else None,
         repository_name=repository_name if repository_name else "",
-        selected_owners=selected_owner if selected_owner else "NO_OWNER",
+        selected_owners=selected_owner,
         owners=owners,
         access_levels=["ADMIN", "OTHER"],
         selected_access_levels=selected_access_levels
         if selected_access_levels
         else ["ADMIN", "OTHER"],
         exclude_other_relationships_where_admins_exist=exclude_other_relationships_where_admins_exist,
+    )
+
+
+@owner_route.route("/<owner>", methods=["GET"])
+@requires_auth
+def owner_dashboard(owner: str):
+    database_service = DatabaseService()
+    repositories = database_service.find_all_repositories()
+    owners = database_service.find_all_owners()
+
+    if owner not in owners:
+        abort(404)
+
+    repositories_with_admin_access = []
+    filtered_repositories_by_owner = []
+    for repository in repositories:
+        for repository_owner in repository["owners"]:
+            if repository_owner["owner_name"] == owner:
+                filtered_repositories_by_owner += [repository]
+
+                if "ADMIN_ACCESS" in repository_owner.get("relationship_type"):
+                    repositories_with_admin_access += [repository]
+
+    return render_template(
+        "pages/owner_dashboard.html",
+        owner=owner,
+        filtered_repositories_by_owner=filtered_repositories_by_owner,
+        all_repositories=repositories,
+        repositories_with_admin_access=repositories_with_admin_access,
     )
